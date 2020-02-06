abc97696f2e048ad90c1ecb89f39320b5613f323
Loïc Montagna <loic.montagna@epita.fr>
2020-02-06 21:33:12 +0100

    Ajout de la caméra
    Ajout de la cam, du script pour être précis, et début des scripts des bâtiment et des joueurs

---
+++ Projet S2/Assets/Prefabs/Bâtiments/Bâtiment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+
+    public enum BuildingType
+    {
+        QG, QG_Captured, Ville, MineDeFer, Caserne, TourDarcher
+    }
+
+    protected BuildingType type;
+
+    public BuildingType Type
+    {
+        get => type;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nothing to do
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //nothing to do
+    }
+}
