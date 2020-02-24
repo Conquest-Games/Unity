370251593ac3cd2fd6cbc182afafa4e71ac33bb1
Theo Ballihaut <theo.ballihaut@epita.fr>
2020-02-24 13:43:33 +0100

    skeleton of Unit

---
+++ Projet S2/Assets/Prefabs/Unit/SiegeWeapon/SiegeWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Soldier
+{
+    public class SiegeWeapon : Unit
+    {
+        #region Attributs
+        
+        private static int[] da = {30, 60, 90};
+        private static int[] hp = {15, 30, 50};
+        private static int sp = 5;
+        protected int lvl; 
+        
+        #endregion
+
+        public SiegeWeapon(SoldierTeam team)
+            : base(SoldierType.Pathfinder, team, da[0], hp[0], sp, 0)
+        {
+            
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Projet S2/Assets/Prefabs/Unit/Unit.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Soldier
+{
+    public class Unit : MonoBehaviour
+    {
+        
+        #region Attributs
+
+        protected int damage;
+        protected int hp;
+        protected int speed;
+        protected int lvl; 
+        private SoldierType type;
+        private SoldierTeam team;
+
+        public enum SoldierType
+        {
+            Pathfinder, Infantry, Archer, Cavalry, SiegeWeapon
+        }
+
+        public enum SoldierTeam
+        {
+            Blue, Red, Yellow, Green
+        }
+        public SoldierType Type
+        {
+            get { return type; }
+        }
+        public SoldierTeam Team
+        {
+            get { return team; }
+        }
+        
+        #endregion
+
+        
+
+
+        public Unit(SoldierType type,SoldierTeam team, int damage, int hp, int speed, int lvl )
+        {
+            this.type = type;
+            this.team = team;
+            this.damage = damage;
+            this.hp = hp;
+            this.speed = speed;
+            this.lvl = lvl;
+        }
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
