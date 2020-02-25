d6b21cb480add57357fa0647d7b0e191565a8883
Loïc Montagna <loic.montagna@epita.fr>
2020-02-25 19:36:43 +0100

    Début de l'ajout du Menu
    Ajout des principaux boutons, et des fichier de Log

---
+++ Projet S2/Assets/Prefabs/Menu principal/Scripts/Quitter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitter : MonoBehaviour
+{
+    public void Exit()
+    {
+        Debug.Log("Le joueur a quitté le Jeux");
+        Application.Quit();
+    }
+}
