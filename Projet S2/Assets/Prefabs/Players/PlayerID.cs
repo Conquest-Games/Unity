82d8525bf1292e58b6f41918a99fc52374551316
Loïc Montagna <loic.montagna@epita.fr>
2020-02-25 16:18:21 +0100

    ajout de la classe joueur

--- Projet S2/Assets/Prefabs/Players/PlayerID.cs
+++ Projet S2/Assets/Prefabs/Players/PlayerID.cs
@@ -4,6 +4,75 @@ using UnityEngine;
 
 public class PlayerID : MonoBehaviour
 {
+
+    #region Varriables
+
+    int playerNumber;
+    bool is_ingame;
+    bool is_computer;
+    int or;
+    int fer;
+
+    public int IncomeOr;
+    public int IncomeFer;
+
+    #endregion
+
+    #region Getters
+
+    public int Or
+    {
+        get => or;
+    }
+
+    public int Fer
+    {
+        get => fer;
+    }
+
+    #endregion
+
+    #region Constructeurs
+
+    public Player (int number, int numberOfPlayer, int maxPlayer)
+    {
+        this.playerNumber = number;
+        this.is_computer = false;
+        this.is_ingame = true;
+        if (number > maxPlayer)
+        {
+            this.is_ingame = false;
+        }
+        if (number > numberOfPlayer)
+        {
+            this.is_computer = true;
+        }
+    }
+
+    public Initialisate (int incomeOr, int incomeFer)
+    {
+        this.or = 100;
+        this.fer = 0;
+        this.IncomeFer = incomeFer;
+        this.IncomeOr = incomeOr;
+    }
+
+    #endregion
+
+    #region Ressources Actions
+
+    public void RemoveOr(int costOr)
+    {
+        or -= costOr;
+    }
+
+    public void RemoveFer(int costFer)
+    {
+        fer -= costFer;
+    }
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +82,7 @@ public class PlayerID : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        or += IncomeOr;
+        fer += IncomeFer;
     }
 }
