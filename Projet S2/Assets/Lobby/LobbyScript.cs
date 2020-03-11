171139efdab8ae54b7aeb53cf19c3f0d9b635418
Theo Ballihaut <theo.ballihaut@epita.fr>
2020-03-11 21:53:16 +0100

    add multi

---
+++ Projet S2/Assets/Lobby/LobbyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+public class LobbyScript : MonoBehaviour
+{
+    public void NbPlayers()
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
