51956b5edee781988992deba6075f2074566a0d4
Theo Ballihaut <theo.ballihaut@epita.fr>
2020-04-14 14:06:21 +0200

    disable option menu

--- Worlds Conqueror/Assets/Prefabs/Players/test.cs
+++ Worlds Conqueror/Assets/Prefabs/Players/test.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     Player player1 = new Player(1, 2, 4);
-    //player1.RemoveOr(10);
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //player1.RemoveOr(10);
     }
 
     // Update is called once per frame
