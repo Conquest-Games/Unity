c97cfd8210ff23c0839fc8a926ca2fe5e27665eb
Loïc Montagna <loic.montagna@epita.fr>
2020-04-17 15:51:48 +0200

    travail sur l'interface du joueur + ajout du code en commentaire de lucas sur la tour d'archer

--- Worlds Conqueror/Assets/Prefabs/Bâtiments/Tour D'archer/TourDarcherScript.cs
+++ Worlds Conqueror/Assets/Prefabs/Bâtiments/Tour D'archer/TourDarcherScript.cs
@@ -6,22 +6,51 @@ namespace Building
 {
     public class TourDarcherScript : MonoBehaviour
     {
-
-        private static int initLvl = 0;
+        /*
+        private static int lvl = 0;
         private static Building.BuildingTeam team = Building.BuildingTeam.Neutral;
+
+        private static bool target = false;
+        public static bool istarget = false;
+        private static bool isdead = false;
+        private static int[] hp = {500, 1000, 1500};
+        private static int[] dmg = {15, 20, 25};
+        private static int[] upgrade_cost = {100, 200, 300}; //mettre des valeurs
 
         Building ville = new Building(Building.BuildingType.TourDarcher, initLvl, team);
 
         // Start is called before the first frame update
         void Start()
         {
-
+            
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (target == true)
+            {
+                
+            }
 
+            if (istarget == true)
+            {
+                //hp[lvl] -= unit.dmg
+            }
+
+            if (hp <= 0)
+            {
+                isdead = true;
+            }
         }
+
+        void Upgrade()
+        {
+            if (player.money >= upgrade_cost[lvl])
+            {
+                player.money -= upgrade_cost[lvl];
+                lvl += 1;
+            }
+        }*/
     }
 }
--- Worlds Conqueror/Assets/Prefabs/Players/PlayerConstructeur.cs
+++ Worlds Conqueror/Assets/Prefabs/Players/PlayerConstructeur.cs
@@ -16,6 +16,22 @@ namespace Joueur
 
         #endregion
 
+        #region Text déclarateur
+        
+        public Text p1Or;
+        public Text p1Fer;
+
+        public Text p2Or;
+        public Text p2Fer;
+
+        public Text p3Or;
+        public Text p3Fer;
+
+        public Text p4Or;
+        public Text p4Fer;
+        
+        #endregion
+
         // Start is called before the first frame update
         void Start()
         {
@@ -25,7 +41,21 @@ namespace Joueur
         // Update is called once per frame
         void Update()
         {
+            #region Text update
 
+            p1Or.text = p1.Or.ToString();
+            p1Fer.text = p1.Fer.ToString();
+
+            p2Or.text = p2.Or.ToString();
+            p2Fer.text = p2.Fer.ToString();
+
+            p3Or.text = p3.Or.ToString();
+            p3Fer.text = p3.Fer.ToString();
+
+            p4Or.text = p4.Or.ToString();
+            p4Fer.text = p4.Fer.ToString();
+
+            #endregion
         }
     }
 }
