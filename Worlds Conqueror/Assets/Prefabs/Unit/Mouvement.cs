6bdcd422d5ecc6284cb2aea4d6926df748a4b191
Theo Ballihaut <theo.ballihaut@epita.fr>
2020-04-19 12:15:11 +0200

    on peut faire spawner l'infentry en appuyant sur droite mais elle ne bouge pas 8'(

--- Worlds Conqueror/Assets/Prefabs/Unit/Mouvement.cs
+++ Worlds Conqueror/Assets/Prefabs/Unit/Mouvement.cs
@@ -8,22 +8,32 @@ using WorldConqueror;
 
 public class Mouvement : MonoBehaviour
 {
-    /*
+    
     public GameObject unit;
 
+    private bool on = true;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 spawnPos = new Vector3(-95, 0, -75);
+        Vector3 spawnPos = new Vector3(-138.4f, 2, -150);
         Instantiate(unit, spawnPos, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }*/
-    
+        if (on)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 5, Space.Self);
+            if (transform.position.x > 138)
+                on = false;
+            if (transform.position.x > -125 && transform.position.x < -110)
+                transform.Translate(Vector3.forward * Time.deltaTime * 5, Space.Self);
+            if (transform.position.x > 110 && transform.position.x < 125)
+                transform.Translate(-Vector3.forward * Time.deltaTime * 5, Space.Self);
+        }
+    }
+
     /*
     private Player _player = PhotonNetwork.LocalPlayer;
 
---
+++ Worlds Conqueror/Assets/Prefabs/Unit/MoveTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WorldConqueror;
+
+public class MoveTest : MonoBehaviour
+{
+    /*
+    private bool on = false;
+    Unit TheOne = new Infantry(Unit.SoldierTeam.Blue);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject unit = GameObject.Find("Infantry");
+        Vector3 spawnPos = new Vector3(-138.4f, 2, -150);
+        Instantiate(unit, spawnPos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("i"))
+        {
+            
+            on = true;
+        }
+
+        if (on)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * TheOne.Speed, Space.Self);
+            if (transform.position.x > 138)
+                on = false;
+            if (transform.position.x > -125 && transform.position.x < -110)
+                transform.Translate(Vector3.forward * Time.deltaTime * TheOne.Speed, Space.Self);
+            if (transform.position.x > 110 && transform.position.x < 125)
+                transform.Translate( - Vector3.forward * Time.deltaTime * TheOne.Speed, Space.Self);
+
+        }
+    }*/
+}
