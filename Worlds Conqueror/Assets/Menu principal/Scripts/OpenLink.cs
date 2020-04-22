2c48e5e9bce008fec3831ecca7e1de1814196638
Loïc Montagna <loic.montagna@epita.fr>
2020-04-22 11:21:40 +0200

    ajout du lien vers le site
    ajout de la redirection vers le site sur le bouton site internet du menu principal

---
+++ Worlds Conqueror/Assets/Menu principal/Scripts/OpenLink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void SiteInternet()
+    {
+        Application.OpenURL("https://worldsconqueror.000webhostapp.com/#");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
