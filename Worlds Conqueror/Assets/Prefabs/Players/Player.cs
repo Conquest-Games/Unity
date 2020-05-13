b411bfb35db5c7cfc29bd5d97d8c0b7c02e9711a
Loïc Montagna <loic.montagna@epita.fr>
2020-05-13 16:03:32 +0200

    AJout de la gestion de ressource
    Ajout de fonction, de l'affichage de l'income, etc

--- Worlds Conqueror/Assets/Prefabs/Players/Player.cs
+++ Worlds Conqueror/Assets/Prefabs/Players/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Joueur
 {
@@ -9,11 +10,19 @@ namespace Joueur
 
         #region Varriables
 
-        private int or;
-        private int fer;
-        private int incomeFer;
-        private int incomeOr;
+        private int or = 100;
+        private int fer = 0;
+        private int incomeFer = 100;
+        private int incomeOr = 500;
 
+        #endregion
+
+        #region Text déclarateur
+
+        public Text pOr;
+        public Text pFer;
+        public Text pOrIncome;
+        public Text pFerIncome;
 
         #endregion
 
@@ -31,28 +40,52 @@ namespace Joueur
 
         #endregion
 
-        #region Constructeurs
-
-        public Player()
-        {
-            or = 100;
-            fer = 0;
-            incomeFer = 0;
-            incomeOr = 0;
-        }
-
-        #endregion
-
         #region Ressources Actions
 
-        public void AjoutOr(int costOr)
+        //Actualise les ressource avec l'income de chaque ressource
+        public void AddIncome()
         {
-            or += costOr;
+            if (or + incomeOr >= 9999)
+                or = 9999;
+            else
+                or += incomeOr;
+
+            if (fer + incomeFer >= 9999)
+                fer = 9999;
+            else
+                fer += incomeFer;
         }
 
-        public void AjoutFer(int costFer)
+        //Ajoute amount a Or
+        public void AjoutOr(int amount)
         {
-            fer += costFer;
+            or += amount;
+        }
+
+        //Ajoute amount a Fer
+        public void AjoutFer(int amount)
+        {
+            fer += amount;
+        }
+
+        //Retire cout et renvoi true si le cout est inferieurs au ressourcers, sinon renvoi false et le nom de la ressource qui manque
+        public (bool, string) Cout(int costOr, int costFer = 0)
+        {
+            if (or < costOr && fer < costFer)
+            {
+                return (false, "Pas assez d'Or et de Fer");
+            }
+            if (or < costOr)
+            {
+                return (false, "Pas assez d'Or");
+            }
+            if (fer < costFer)
+            {
+                return (false, "Pas assez de Fer");
+            }
+            or -= costOr;
+            fer -= costFer;
+            return (true, "");
         }
 
         #endregion
@@ -60,13 +93,29 @@ namespace Joueur
         // Start is called before the first frame update
         void Start()
         {
+            InvokeRepeating("AddIncome", 0f, 1);
 
+            #region Text update
+
+            pOr.text = or.ToString();
+            pFer.text = fer.ToString();
+            pOrIncome.text = "+" + incomeOr.ToString() + "/s";
+            pFerIncome.text = "+" + incomeFer.ToString() + "/s";
+
+            #endregion
         }
 
         // Update is called once per frame
         void Update()
         {
+            #region Text update
 
+            pOr.text = or.ToString();
+            pFer.text = fer.ToString();
+            pOrIncome.text = "+" + incomeOr.ToString() + "/s";
+            pFerIncome.text = "+" + incomeFer.ToString() + "/s";
+
+            #endregion
         }
     }
 }
