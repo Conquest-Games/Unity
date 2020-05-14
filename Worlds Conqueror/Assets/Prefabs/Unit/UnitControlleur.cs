9ecdfd87431d98e3f2a8742dd3563e11305a1648
Loïc Montagna <loic.montagna@epita.fr>
2020-05-14 17:25:52 +0200

    changement interface+ commencé les fct d'upgrade des unités

---
+++ Worlds Conqueror/Assets/Prefabs/Unit/UnitControlleur.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldConqueror
+{
+    public class UnitControlleur : MonoBehaviour
+    {
+        //public void Upgrade
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
